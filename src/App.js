b6d15fa86e76fffdbcda3b8b590abda640e1241c
nikhil076 <nikhil.arvind7@gmail.com>
2021-10-24 22:26:00 +0530

    AD

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import  logo from '../assets/logo.png'
+import logo from './assets/logo.png'
 
 class App extends React.Component {
 
@@ -8,7 +8,7 @@ class App extends React.Component {
     constructor() {
         super();
         this.state =  {
-            title : 'Hello from Nick'
+            userName : ''
         }
     }
 
@@ -17,12 +17,17 @@ class App extends React.Component {
         console.log("save button is clicked", $event);
         window.open(this.url, "_blank");
     }
+    onNameChange = (event) => {
+        console.log("value is" , event.target.value);
+        //set the title using setState method
+        this.state({userName: event.target.value})
+    }
 
     render() {
         return (
             <div>
-                <h1>{this.state.title}</h1>
-                <img src="{logo}" onClick={this.onClick} alt="The Bridgelabz logo : a Bridge to employment through lab works" />
+                <h1>{this.state.userName} from Bridgelabz</h1>
+                <img src={logo} onClick={this.onClick} alt="The Bridgelabz logo : a Bridge to employment through lab works" />
             </div>
         )
     }
